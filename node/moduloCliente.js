cb1776d5197aadf9aee4ec715b5d35f49a68eb96
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-12-17 16:08:50 -0400

    Node: Sistema de Módulos

---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const modulaA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(modulaA.ola)
+console.log(modulaA.bemVindo)
+console.log(modulaA.ateLogo)
+console.log(modulaA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
