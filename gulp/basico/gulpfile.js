ad4c22e97af17fa5880fec7c645b3957de1b791e
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-12-31 15:12:37 -0400

    Gulp: fundamentos

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,1 +1,31 @@
+const gulp = require('gulp')
 
+gulp.task('default', () => {
+  gulp.start('copiar', 'fim')
+})
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+  // gulp.src('pastaA/**/*.txt')
+  gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    // .pipe(trasformacao1())
+    // .pipe(transformacao2())
+    .pipe(gulp.dest('pastaB'))
+})
+
+gulp.task('antes1', () => {
+  console.log('Antes 1!!!')
+})
+
+gulp.task('antes2', () => {
+  console.log('Antes 2!!!')
+})
+
+gulp.task('fim', ['fim1', 'fim2'])
+
+gulp.task('fim1', () => {
+  console.log('Fim1!!!')
+})
+
+gulp.task('fim2', () => {
+  console.log('Fim2!!!')
+})
