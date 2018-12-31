d8097b766f89eac1b8bbd1ff536efcc77810b928
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-12-31 18:20:28 -0400

    Gulp: Processando Javascript

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -1,1 +1,19 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
 
+gulp.task('default', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(babel({
+      // minified: true, // or uglify
+      comments: false,
+      presets: ["env"]
+    }))
+    .pipe(uglify())
+    .on('error', function (err) {
+      console.log(err)
+    })
+    .pipe(concat('codigo.min.js'))
+    .pipe(gulp.dest('build'))
+})
