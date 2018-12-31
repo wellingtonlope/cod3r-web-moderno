7011e2f498643a3a7f35b5c6b7abd57f546a9df7
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-12-31 12:39:36 -0400

    Ajax: fetch

--- ajax/server.js
+++ ajax/server.js
@@ -32,4 +32,11 @@ app.post('/upload', (req, res) => {
   })
 })
 
+app.post('/formulario', (req, res) => {
+  res.send({
+    ...req.body,
+    id: 1
+  })
+})
+
 app.listen(8080, () => console.log('Executando...'))
