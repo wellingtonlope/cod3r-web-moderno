0ef3c959dc617c1a34876eb90a27d32a2c705f21
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-04 02:29:55 -0400

    React: Componente Com Parâmetro

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReacDOM from 'react-dom'
 
-import Primerio from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
-ReacDOM.render(<Primerio />, document.getElementById('root'))
+ReacDOM.render(<BomDia nome="Guilherme" />, document.getElementById('root'))
