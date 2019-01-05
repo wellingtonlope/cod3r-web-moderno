093a517f6b39ab18707126399be17bc1b16297f8
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-01-05 17:26:09 -0400

    Calculadora Vue: Criando Componentes

---
+++ vue/calculator/src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="button">0</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ vue/calculator/src/components/Display.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="display">0</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
